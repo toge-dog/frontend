881ec53f73befaa66e8a9f05810313cbe6b1e72c
gwanghui97 <lgh970409@gmail.com>
2024-08-19 02:06:23 +0900

    feat:#15 Not Found Page 만들기

--- src/App.js
+++ src/App.js
@@ -11,23 +11,29 @@ import FindPasswordResetPage from './pages/FindPage/FindPasswordResetPage';
 import SignUpTermsPage from './pages/SignUp/SignUpTermsPage'
 import SignUpInfoPage from './pages/SignUp/SignUpInfoPage'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NotFoundPage from './pages/ErrorPage/NotFoundPage';
 
 function App() {
   return (
     <Routes>
+
       <Route path='/' element={<AppLayout />}>
         <Route index element={<HomePage />}/>
         <Route path="boards">
           <Route path=":boardType" element={<BoardPage />}/>
         </Route>
+
         <Route path="login" element={<LoginPage />}/>
         <Route path="sign-up">
           <Route path="" element={<SignUpTermsPage />} />
-          <Route path="info" element={<SignUpInfoPage />} />
+          <Route path="" element={<SignUpInfoPage />} />
         </Route>
+
         <Route path="members/find-id" element={<FindIdPage />}/>
         <Route path="members/find-pw" element={<FindPasswordInitPage />}/>
         <Route path="members/find-pw/:memberId" element={<FindPasswordResetPage />}/>
+
+        <Route path="*" element={<NotFoundPage />}/>
       </Route>
     </Routes>
   );
